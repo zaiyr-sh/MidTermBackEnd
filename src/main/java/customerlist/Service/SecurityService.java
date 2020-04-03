545e74f8f53eaddb8f644d338d371e528edcf1de
Zaiyr <zaiyr.00@gmail.com>
2020-04-03 17:54:57 +0600

    customize seccurity

---
+++ src/main/java/customerlist/Service/SecurityService.java
@@ -0,0 +1,30 @@
+package customerlist.Service;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityService extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().
+                authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/**").access("hasRole('ADMIN CUSTOMERS')")
+                .and()
+                .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}admin")
+                .roles("ADMIN CUSTOMERS");
+    }
+
+}
